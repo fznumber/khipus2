e6b0cabdab1e2debcecf7f7d5346f247257a3ea8
diego <diego.loza.click@gmail.com>
2015-03-26 22:41:31 -0400

    actualizando la BD

---
+++ sql/id_gen.sql
@@ -0,0 +1,5 @@
+INSERT INTO eos.id_gen (GEN_NAME, GEN_VAL) VALUES ('ArticulosPedido_Gen', 250);
+INSERT INTO eos.id_gen (GEN_NAME, GEN_VAL) VALUES ('PedidosCodigo_Gen', 4);
+INSERT INTO eos.id_gen (GEN_NAME, GEN_VAL) VALUES ('Pedidos_Gen', 5);
+INSERT INTO eos.id_gen (GEN_NAME, GEN_VAL) VALUES ('Persona_GEN', 18);
+INSERT INTO eos.id_gen (GEN_NAME, GEN_VAL) VALUES ('Territoriotrabajo_Gen', 5);
---
+++ sql/tipocliente.sql
@@ -0,0 +1,5 @@
+INSERT INTO eos.tipocliente (IDTIPOCLIENTE, NOMBRE) VALUES (1, 'Tienda');
+INSERT INTO eos.tipocliente (IDTIPOCLIENTE, NOMBRE) VALUES (2, 'Supermercado');
+INSERT INTO eos.tipocliente (IDTIPOCLIENTE, NOMBRE) VALUES (3, 'Micromercado');
+INSERT INTO eos.tipocliente (IDTIPOCLIENTE, NOMBRE) VALUES (4, 'Desayuno Escolar');
+INSERT INTO eos.tipocliente (IDTIPOCLIENTE, NOMBRE) VALUES (5, 'Agencia');
